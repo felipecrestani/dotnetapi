f53bfb079d737bf335da8e25a850d0bdf9be3ad3
felipecrestani <fcrestanihc@hotmail.com>
2019-04-09 23:23:02 -0300

    first

---
+++ Controllers/MainController.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace dotnetapi.Controllers
+{
+    [Route("/")]
+    [ApiController]
+    public class MainController : ControllerBase
+    {
+        [HttpGet]
+        public ActionResult<string> Get()
+        {
+            return  ".net Core";
+        }
+    }
+}
